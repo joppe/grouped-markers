7cc4f9d0a1b133bbffe2e857657126f3ca9dfbed
Joppe Aarts <joppe@apestaartje.info>
2015-11-30 19:14:23 +0100

    created a google maps loader for systemjs

--- demo/js/system.conf.js
+++ demo/js/system.conf.js
@@ -5,7 +5,18 @@ System.config({
 
     transpiler: 'babel',
 
+    map: {
+        'google-maps-loader': 'dist/helper/google-map-loader.js'
+    },
+
     paths: {
-        'image-preloader/*': 'src/image-preloader/*.js'
+        'google-maps': 'http://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&language=nl'
+    },
+
+    meta: {
+        'google-maps': {
+            build: false,
+            loader: 'google-maps-loader'
+        }
     }
 });
