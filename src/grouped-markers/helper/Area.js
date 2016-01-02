9b8d1ee11da271c197e54ba6f85dc54627b6e84c
Joppe Aarts <joppe@apestaartje.info>
2016-01-02 16:07:46 +0100

    Area supports removing a point

--- src/grouped-markers/helper/Area.js
+++ src/grouped-markers/helper/Area.js
@@ -19,38 +19,88 @@ export class Area {
     }
 
     /**
+     * @returns {Number}
+     */
+    get length() {
+        return this.points.length;
+    }
+
+    /**
      * @param {Number} size
      */
     constructor(size = 0) {
         this.size = size;
-        this.count = 0;
+        this.points = [];
         this.center = new Point(0, 0);
     }
 
     /**
-     * Add a point and recalculate the average position and set it as the center.
+     * Add a point and recalculate the center.
      *
      * @param {Point} point
      * @returns {Area}
      */
     addPoint(point) {
-        let x = this.center.x,
-            y = this.center.y;
+        this.points.push(point);
 
-        this.count += 1;
-
-        x *= this.count - 1;
-        y *= this.count - 1;
-
-        x += point.x;
-        y += point.y;
-
-        x /= this.count;
-        y /= this.count;
-
-        this.center.x = x;
-        this.center.y = y;
+        this.calculateCenter();
 
         return this;
+    }
+
+    /**
+     * Remove a point and recalculate the center.
+     *
+     * @param {Point} point
+     * @returns {Area}
+     */
+    removePoint(point) {
+        let index = this.getIndex(point);
+
+        if (-1 === index) {
+            throw `Point not found "${point.toString()}"`;
+        }
+
+        this.points.splice(index, 1);
+
+        this.calculateCenter();
+
+        return this;
+    }
+
+    /**
+     * Get the index of the first found point that matches the x and y of the given point.
+     *
+     * @param {Point} point
+     * @returns {number}
+     */
+    getIndex(point) {
+        let index = -1;
+
+        this.points.every((p, i) => {
+            let ret = true;
+
+            if (point.x === p.x && point.y === p.y) {
+                ret = false;
+                index = i;
+            }
+
+            return ret;
+        });
+
+        return index;
+    }
+
+    calculateCenter() {
+        let x = 0,
+            y = 0;
+
+        this.points.forEach((point) => {
+            x += point.x;
+            y += point.y;
+        });
+
+        this.center.x = x / this.points.length;
+        this.center.y = y / this.points.length;
     }
 }
