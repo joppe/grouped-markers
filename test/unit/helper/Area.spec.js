28d5ca982041a3e318176ad89f04244e76dbc5f0
Joppe Aarts <joppe@apestaartje.info>
2016-01-02 16:42:39 +0100

    Extra tests added for Area spec

--- test/unit/helper/Area.spec.js
+++ test/unit/helper/Area.spec.js
@@ -48,5 +48,34 @@ describe('Area', () => {
 
         expect(a.center.x).toBe(2);
         expect(a.center.y).toBe(2);
+
+        a.removePoint(new Point(4, 4));
+
+        expect(a.center.x).toBe(1);
+        expect(a.center.y).toBe(1);
+    });
+
+    it('Find a points index', () => {
+        let a = new Area();
+
+        a.addPoint(new Point(1, 1));
+        a.addPoint(new Point(1, 1));
+        a.addPoint(new Point(4, 4));
+
+        expect(a.getIndex(new Point(1, 1))).toBe(0);
+    });
+
+    it('Get the number of points', () => {
+        let a = new Area();
+
+        expect(a.length).toBe(0);
+        a.addPoint(new Point(1, 1));
+        expect(a.length).toBe(1);
+        a.addPoint(new Point(1, 1));
+        expect(a.length).toBe(2);
+        a.addPoint(new Point(4, 4));
+        expect(a.length).toBe(3);
+        a.removePoint(new Point(4, 4));
+        expect(a.length).toBe(2);
     });
 });
