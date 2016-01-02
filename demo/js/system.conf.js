5e3f5cad74bdf5b4457dfd51bf56e7e4322da46c
Joppe Aarts <joppe@apestaartje.info>
2016-01-02 16:49:11 +0100

    systemjs config

--- demo/js/system.conf.js
+++ demo/js/system.conf.js
@@ -6,15 +6,16 @@ System.config({
     transpiler: 'babel',
 
     map: {
-        'google-maps-loader': 'vendor/google-maps-loader/dist/google-maps-loader.js',
+        'google-maps-loader': 'dist/vendor/google-maps-loader/dist/google-maps-loader.js',
         underscore: 'dist/vendor/underscore/underscore.js',
         backbone: 'dist/vendor/backbone/backbone.js',
         react: 'dist/node_modules/react/dist/react.js',
         'react-dom': 'dist/node_modules/react-dom/dist/react-dom.js',
-        jquery: 'dist/vendor/jquery/dist/jquery.js',
+        jquery: 'dist/vendor/jquery/dist/jquery.js'
     },
 
     paths: {
+        'grouped-markers/*': 'dist/grouped-markers/*',
         'google-maps': 'http://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&language=nl'
     },
 
