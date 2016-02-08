d9bb781539b3fe4ba288276223ce5e7f2755b8df
Joppe Aarts <joppe@apestaartje.info>
2016-02-08 21:27:53 +0100

    bundle script added

---
+++ bundle.js
@@ -0,0 +1,28 @@
+/*global require, console*/
+
+var Builder = require('systemjs-builder'),
+    builder = new Builder();
+
+builder.config({
+    transpiler: 'babel'
+});
+
+builder
+    .buildStatic('src/grouped-markers/*', 'dist/grouped-markers.min.js', {
+        minify: true,
+        sourceMaps: true,
+        format: 'amd'
+    })
+    .then(function () {
+        'use strict';
+
+        console.log('Build complete');
+    })
+    .catch(function (err) {
+        'use strict';
+
+
+        console.log('Build error');
+        console.log(err);
+    })
+;
